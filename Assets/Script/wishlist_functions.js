32d42d9d16b8307359b4f5e6bf7c16618e3ff944
Hugo Ernesto <mail.hbler@gmail.com>
2022-03-22 16:01:02 -0300

    update: another fix

--- Assets/Script/wishlist_functions.js
+++ Assets/Script/wishlist_functions.js
@@ -51,7 +51,9 @@ function addWish() {
 
 /// remove from Whishes
 function cancelWish() {
-  const product = this.parentElement.parentElement.childNodes[0].innerText;
+  const product =
+    this.parentElement.childNodes[0].innerText ||
+    this.parentElement.parentElement.childNodes[0].innerText;
   let index = Wishes.indexOf(`P${product}`);
   Wishes.splice(index, 1);
   showWishes();
